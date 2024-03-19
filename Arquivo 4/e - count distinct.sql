258668a7a8966746e1f9aaaf39922c2c680c1892
Paolo Klahold <105084677+PaoloKlahold@users.noreply.github.com>
2024-03-19 15:29:31 -0300

    Add files via upload

--- Arquivo 4/e - count distinct.sql
+++ Arquivo 4/e - count distinct.sql
@@ -1,7 +1,10 @@
 use treinamento
 -- todos pediram 27 produtos distintos, mas alguns pediram mais do(s) mesmo(s) produto(s) que outros  
-SELECT c.NMCUSTOMER, count(distinct pr.CDPRODUCT) as 'produtosDistintos', sum(VLUNITARY) as 'SomaDosValores', VAR(VLUNITARY) as 'MediaDosValores'
+SELECT c.NMCUSTOMER, count(distinct pr.CDPRODUCT) as 'produtosDistintos', 
+					cast((sum(pr.VLUNITARY * pr.QTAMOUNT)) as  decimal(10,2)) as 'SomaDosValores',
+					cast((VAR(VLUNITARY * pr.QTAMOUNT)) as  decimal(10,2)) as 'MediaDosValores'
 	FROM CUSTOMER as c INNER JOIN REQUEST as r ON c.CDCUSTOMER = r.CDCUSTOMER
 					   INNER JOIN PRODUCTREQUEST as pr ON r.CDREQUEST = pr.CDREQUEST
 					   group by c.NMCUSTOMER
 					   order by count(pr.CDPRODUCT) DESC
+
--- Arquivo 4/h - 4Inner group.sql
+++ Arquivo 4/h - 4Inner group.sql
@@ -2,7 +2,7 @@ use treinamento
 
 SELECT  c.NMCUSTOMER as 'NomeCliente', 
 		p.NMPRODUCT as 'NomeProduto',
-		cast((sum(pr.VLUNITARY)) as decimal(6,2)) as 'ValorSoma',
+		cast((sum(pr.VLUNITARY) * sum(QTAMOUNT)) as decimal(10,2)) as 'ValorSoma',
 		count(pr.CDPRODUCT) as 'ContVezesProduto',
 		s.NMSUPPLIER as 'NomeFornecedor'
 		FROM CUSTOMER as c INNER JOIN REQUEST as r ON c.CDCUSTOMER = r.CDCUSTOMER
@@ -10,6 +10,5 @@ SELECT  c.NMCUSTOMER as 'NomeCliente',
 						   INNER JOIN PRODUCT as p ON pr.CDPRODUCT = p.CDPRODUCT
 						   INNER JOIN SUPPLIER as s ON p.CDSUPPLIER = s.CDSUPPLIER
 						   GROUP BY c.NMCUSTOMER, p.NMPRODUCT, s.NMSUPPLIER
-						   HAVING sum(pr.VLUNITARY) >= 1000
+						   HAVING sum(pr.VLUNITARY) * sum(QTAMOUNT) >= 1000
 						   order by c.NMCUSTOMER, p.NMPRODUCT
--- não há clientes que gastaram mais de 1000 em um só produto
